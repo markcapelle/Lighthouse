3117007be63aa441b52e3eddbaf263aa0fa37ef1
Mark Van Capelle <markcapelle@outlook.ie>
2026-09-06 18:14:56 +0100

    FEAT: login/logout and nav styles
    Adjusted nav styles on both web-nav and app-nav.
    implemented login and logout.

--- main/urls.py
+++ main/urls.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
 from django.urls import path
 from django.shortcuts import render
+from users.views import login_view, logout_view, dashboard_view
 
 def index(request):
     return render(request, 'index.html')
 
 urlpatterns = [
     path('', index, name='index'),
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
+    path('dashboard/', dashboard_view, name='dashboard'),
     path('admin/', admin.site.urls),
 ]
